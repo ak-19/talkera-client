180a60353a153669acd0e3b8ad5a040878f3df33
ante <ante.kotarac@gmail.com>
2022-12-09 11:10:56 +0100

    styling header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,8 +3,11 @@ import { Fragment } from 'react';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
 
 function Header(props) {
+  const user = JSON.parse(localStorage.getItem('current_user'))
+
   return (
     <Fragment>
       <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
@@ -16,13 +19,32 @@ function Header(props) {
           noWrap
           sx={{ flex: 1 }}
         >
-          Talkera
-        </Typography>
-        <Button variant="outlined" size="small">
-          Sign in
-        </Button>
-      </Toolbar>
+          <Typography
+            component="a"
+            variant="h3"
+            href="/"
+            sx={{
+              mr: 2,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none',
+              flexGrow: 1
+            }}>
+            Talkera
+          </Typography>
 
+        </Typography>
+        {user ? (
+          <Button component={Link} variant="outlined" size="small" to="/auth">
+            Logout
+          </Button>
+        ) : (
+          <Button component={Link} variant="outlined" size="small" to="/auth">
+            Sign in
+          </Button>
+        )}
+      </Toolbar>
     </Fragment>
   );
 }
